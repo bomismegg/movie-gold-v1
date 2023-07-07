6f71ce04ce3d6f708823d46e802eae32f69cd29d
bomismegg <minhtri28102003@gmail.com>
2023-07-07 22:54:30 +0700

    2nd-commit

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://4d6e-116-100-44-169.ngrok-free.app',
+    baseURL:'https://653b-115-73-216-91.ngrok-free.app',
     headers:{"ngrok-skip-browser-warning":"true"}
 });
